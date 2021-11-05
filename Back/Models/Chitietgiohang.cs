eebee7cec8c856ea44d51f45775ffcc99be08b16
chaupha180801 <chaupha180801@gmail.com>
2021-11-05 21:34:19 +0700

    Add models

---
+++ Back/Models/Chitietgiohang.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Back.Models
+{
+    public class Chitietgiohang
+    {
+        public string Magiohang { get; set; }
+        public string Masanpham { get; set; }
+        public int? Soluong { get; set; }
+
+        public Sanpham MasanphamNavigation { get; set; }
+    }
+}
---
+++ Back/Models/Chitietsanpham.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Back.Models
+{
+    public class Chitietsanpham
+    {
+        public Chitietsanpham()
+        {
+            Baohanhs = new HashSet<Baohanh>();
+            Chitiethoadons = new HashSet<Chitiethoadon>();
+        }
+
+        public string Imei { get; set; }
+        public string Masanpham { get; set; }
+        public DateTime? Ngaysanxuat { get; set; }
+        public string Tinhtrang { get; set; }
+
+        public Sanpham MasanphamNavigation { get; set; }
+        public ICollection<Baohanh> Baohanhs { get; set; }
+        public ICollection<Chitiethoadon> Chitiethoadons { get; set; }
+    }
+}

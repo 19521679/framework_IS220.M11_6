861f3c4bcbe596f3b5b7b95935bf1ff860117144
Khang Nguyễn Lê Nguyên <19521666@ms.uit.edu.vn>
2021-11-17 23:47:19 +0700

    update model

--- Back/Models/NhanVienModel.cs
+++ Back/Models/NhanVienModel.cs
@@ -6,29 +6,32 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Back.Models
 {
-    public partial class KhachHangModel
-    {   
+    public partial class NhanVienModel
+    {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Display(Name = "Mã khách hàng")]
-        public int Makhachhang { get; set; }
-        [Display(Name = "Tên khách hàng")]
-        public string Tenkhachhang { get; set; }
+        [Display(Name = "Mã nhân viên")]
+        public int Manhanvien { get; set; }
+        [Display(Name = "Tên nhân viên")]
+        public string Tennhanvien { get; set; }
         [Display(Name = "Email")]
         public string Email { get; set; }
         [Display(Name = "Số điện thoại")]
         public string Sodienthoai { get; set; }
         [Display(Name = "Địa chỉ")]
         public string Diachi { get; set; }
+        [Display(Name = "Ngày vào làm")]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Ngayvaolam { get; set; }
+        [Display(Name = "CCCD")]
+        public string Cccd { get; set; }
         [Display(Name = "Ngày sinh")]
         [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
-        public DateTime? Ngaysinh { get; set; }
-        [Display(Name = "Loại khách hàng")]
-        public string Loaikhachhang { get; set; }
+        public DateTime Ngaysinh { get; set; }
+        [Display(Name = "Chức vụ")]
+        public string Chucvu { get; set; }
 
-        public  ICollection<DanhGiaModel> Danhgia { get; set; }
-        public  ICollection<GioHangModel> Giohang { get; set; }
         public  ICollection<HoaDonModel> hoaDons { get; set; }
-        public  ICollection<TaiKhoanKhachHangModel> taiKhoanKhachHangs { get; set; }
+        public  ICollection<TaiKhoanNhanVienModel> taiKhoanNhanViens { get; set; }
     }
 }
